f002826e3671caae4f87378782d94ad751b99b1a
RishiTiwari03 <119129580+RishiTiwari03@users.noreply.github.com>
2024-10-14 04:18:36 +0530

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const router = express.Router({ mergeParams: true }); //parent file ki jo id variable hai req.params usko yha tk lane ke liye
-//ya merge krne ke liye we use mergeParams:true
+const router = express.Router({ mergeParams: true }); 
 const wrapAsync = require('../utils/wrapAsync.js');
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 const reviewController = require("../controllers/reviews.js")
